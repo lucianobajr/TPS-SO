6d9601994a96010e866c72718d85f9d5cd24babe
PapaArt <artur.papa@ufv.br>
2022-07-25 20:57:40 -0300

    Up

--- TP02/src/main-2.c
+++ TP02/src/main-2.c
@@ -4,15 +4,11 @@
 
 int main(int argc, char const *argv[])
 {
+    FILE *fpointer;
     generate();
     generate_address();
-    main_memory *memory = init_main_memory();
-    made_address(memory);
     bool allocated = true;
-
-    FILE *fd;
     int open_frame = 0;
-
     int page_faults = 0;
     int TLB_hits = 0;
 
@@ -26,8 +22,8 @@ int main(int argc, char const *argv[])
 
     char phy_mem[PHYS_MEM_SIZE][PHYS_MEM_SIZE];
 
-    fd = fopen("data/address.txt", "r");
-    if (fd == NULL){
+    fpointer = fopen("data/address.txt", "r");
+    if (fpointer == NULL){
         printf("Fail trying to open the file!!!\n");
         exit(0);
     }
@@ -35,84 +31,15 @@ int main(int argc, char const *argv[])
     list allocation_algorithms;
     make_empty_list(&allocation_algorithms);
 
-    int input_type, continue_input;
-
-    do
-    {
-        menu_main_memory();
-        scanf("%d", &input_type);
-
-        if (input_type != 1 && input_type != 2 && input_type != 3 && input_type != 4)
-        {
-            logger("\nError! Invalid input!\n", ERROR_COLOR);
-            printf("----------> ");
-        }
-        else
-        {
-            if (element_already_exists(&allocation_algorithms, input_type) == 1)
-            {
-                logger("\nError! You already chose this option!\n", ERROR_COLOR);
-                printf("----------> ");
-            }
-            else
-            {
-                key tmp;
-                tmp.value = input_type;
-                insert_in_list(&allocation_algorithms, tmp);
-            }
-        }
-
-        printf("Do you want to choose one more algorithm? (1) yes; (2) no: ");
-        scanf("%d", &continue_input);
-    } while (continue_input != 2);
-
-    printf("\n");
     main_memory *memory_ff; // memória principal first fit
-    main_memory *memory_nf; // memória principal next fit
-    main_memory *memory_bf; // memória principal best fit
-    main_memory *memory_wf; // memória principal worst fit
 
     metrics memory_metrics_ff; // métricas first fit
-    metrics memory_metrics_nf; // métricas next fit
-    metrics memory_metrics_bf; // métricas best fit
-    metrics memory_metrics_wf; // métricas worst fit
 
     queue denied_process_ff; // fila de processos negagod => first fit
-    queue denied_process_nf; // fila de processos negagod => next fit
-    queue denied_process_bf; // fila de processos negagod => best fit
-    queue denied_process_wf; // fila de processos negagod => worst fit
 
-    pointer_list aux_pointer_list;
-    aux_pointer_list = allocation_algorithms.first->next;
-    while (aux_pointer_list != NULL)
-    {
-        if (aux_pointer_list->content.value == 1)
-        {
-            memory_ff = init_main_memory();
-            initialize_metrics(&memory_metrics_ff);
-            init_queue(&denied_process_ff);
-        }
-        if (aux_pointer_list->content.value == 2)
-        {
-            memory_nf = init_main_memory();
-            initialize_metrics(&memory_metrics_nf);
-            init_queue(&denied_process_nf);
-        }
-        if (aux_pointer_list->content.value == 3)
-        {
-            memory_bf = init_main_memory();
-            initialize_metrics(&memory_metrics_bf);
-            init_queue(&denied_process_bf);
-        }
-        if (aux_pointer_list->content.value == 4)
-        {
-            memory_wf = init_main_memory();
-            initialize_metrics(&memory_metrics_wf);
-            init_queue(&denied_process_wf);
-        }
-
-        aux_pointer_list = aux_pointer_list->next;
-    }
+    memory_ff = init_main_memory();
+    initialize_metrics(&memory_metrics_ff);
+    init_queue(&denied_process_ff);
 
     int highest_value = highest_value_in_memory(memory_ff);
 
@@ -127,36 +54,21 @@ int main(int argc, char const *argv[])
         printf("VALOR = %d\n", process_size);
 
         first_fit(memory_ff, &memory_metrics_ff, &denied_process_ff, process_size);
-        next_fit(memory_nf, &memory_metrics_nf, &denied_process_nf, process_size, &next_fit_index);
-        best_fit(memory_bf, &memory_metrics_bf, &denied_process_bf, process_size);
-        worst_fit(memory_wf, &memory_metrics_wf, &denied_process_wf, process_size);   
-    
+
         add_external_fragment(&memory_metrics_ff, external_fragments(memory_ff));
-        add_external_fragment(&memory_metrics_nf, external_fragments(memory_nf));
-        add_external_fragment(&memory_metrics_bf, external_fragments(memory_bf));
-        add_external_fragment(&memory_metrics_wf, external_fragments(memory_wf));
     }
 
     print_main_memory(memory_ff);
-    print_main_memory(memory_nf);
-    print_main_memory(memory_bf);
-    print_main_memory(memory_wf);
 
     print_memory_with_metrics(memory_ff, &memory_metrics_ff);
-    print_memory_with_metrics(memory_nf, &memory_metrics_nf);
-    print_memory_with_metrics(memory_bf, &memory_metrics_bf);
-    print_memory_with_metrics(memory_wf, &memory_metrics_wf);
 
     printf("\nLIST DE PROCESSOS NEGADOS\n");
     print_queue(&denied_process_ff);
-    print_queue(&denied_process_bf);
-    print_queue(&denied_process_nf);
-    print_queue(&denied_process_wf);
 
-    // for (int i = 0; i < SIZE; i++)
-    // {
-    //     memory[i].allocated == 0 ? allocated = false : find_page(memory[i].address, page_table, &tlb, (char*)phy_mem, &open_frame, &page_faults, &TLB_hits);
-    // }
+    for (int i = 0; i < SIZE; i++)
+    {
+        memory_ff[i].allocated == 0 ? allocated = false : find_page(memory_ff[i].address, page_table, &tlb, (char*)phy_mem, &open_frame, &page_faults, &TLB_hits);
+    }
     
     return 0;
 }
